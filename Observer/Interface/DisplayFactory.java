3fed5db746d205aff0d909e3198ebe0fd865e65c
jorisSchaller <joris.schaller@heig-vd.ch>
2022-01-19 21:05:51 +0100

    fait un DisplayFactory qui cré une class anonyme qui implémente l'interface

---
+++ Observer/Interface/DisplayFactory.java
@@ -0,0 +1,17 @@
+package Observer.Interface;
+
+public class DisplayFactory {
+    DisplayFactory(){
+    }
+    public DisplayElement generateNewDisplay(){
+        //retourn une class Anonyme qui implémente DisplayElement
+        return new DisplayElement (){
+            @Override
+            public void display() {
+                System.out.println("Ceci est une class anonyme qui implémente Display, " +
+                                           "l'interet dans ce cas est limité :/ ");
+            }
+        };
+    }
+
+}
